b9ed6c01e91c48d39d9747c04df5a99cac11b439
weichenglin1 <weichenglin1@360.cn>
2024-07-30 18:18:00 +0800

    init

--- app/src/main/java/com/game/sdk/demo/DemoMainActivity.java
+++ app/src/main/java/com/game/sdk/demo/DemoMainActivity.java
@@ -2,14 +2,39 @@ package com.game.sdk.demo;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.game.sdk.frame.R;
 
 public class DemoMainActivity extends Activity {
+    int mIndex = 0;
+    TextView mDebugTips;// 测试代码，CP忽略
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mDebugTips = findViewById(R.id.demo_result_tips);
+
+        findViewById(R.id.demo_btn_clear).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mDebugTips.setText("");
+            }
+        });
+
+        // 初始化SDK
+
+    }
+
+    // 测试代码，CP勿接
+    private void appendDebugMsg(String message) {
+        if (mDebugTips.length() <= 0) {
+            mDebugTips.append((mIndex++ + "、") + message);
+        } else {
+            mDebugTips.append("\n" + (mIndex++ + "、") + message);
+        }
     }
 }
